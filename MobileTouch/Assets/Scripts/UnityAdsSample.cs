083d69e78745e864968e6bd21220bdd6305292b0
Bri <bs3864@nyu.edu>
2021-03-21 13:28:53 -0400

    Adding Unity Ad base script

---
+++ MobileTouch/Assets/Scripts/UnityAdsSample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements; // namespace to get advertisment class
+
+public class UnityAdsSample : MonoBehaviour
+{
+    public static bool showAds = true;
+    private string gameID = "";
+    private bool testMode = true; // for debugging ads you would need to keep true until publishing
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //we need to initialize the ads
+        if (!Advertisement.isInitialized)
+        {
+            Advertisement.Initialize(gameID, testMode);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
